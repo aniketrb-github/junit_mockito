b05f2f1ed2f76fb8ba108f8608f8356daf298bff
aniketrb-github <aniketbharsakale@gmail.com>
2019-07-03 21:18:49 +0530

    added code for mockito test case

--- junit-mockito-demo/src/test/java/com/in28mins/business/TodoBusinessImplStubUnitTest.java
+++ junit-mockito-demo/src/test/java/com/in28mins/business/TodoBusinessImplStubUnitTest.java
@@ -1,7 +1,10 @@
 package com.in28mins.business;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -19,6 +22,42 @@ import com.in28mins.data.api.TodoServiceStub;
 public class TodoBusinessImplStubUnitTest {
 
 	@Test
+	void testRetrieveTodosRelatedToSpring_UsingMock() {
+		// creates a mock for this particular interface/class
+		ITodoService todoServiceMock = mock(ITodoService.class);
+		
+		// mock data creation : here an dummy list is created
+		when(todoServiceMock.retrieveTodoList("dummyUser")).thenReturn(Arrays.asList("Spring MVC", "Spring Boot", "Spring Batch", "Hibernate", "MySQL", "AWS"));
+
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		
+		List<String> filteredTodoList = todoBusinessImpl.retrieveTodosRelatedToSpring("dummyUser");
+		
+		assertEquals("should return 3 spring named objects", 3, filteredTodoList.size());
+	}
+	
+	@Test
+	void testRetrieveTodosRelatedToSpring_UsingMockWithEmptyList() {
+		// creates a mock for this particular interface/class
+		ITodoService todoServiceMock = mock(ITodoService.class);
+		
+		// mock data creation : here an empty list is created
+		when(todoServiceMock.retrieveTodoList("dummyUser")).thenReturn(Arrays.asList());
+
+		// using the mock data to feed the Impl class
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		
+		// invoking our main unit testing method
+		List<String> filteredTodoList = todoBusinessImpl.retrieveTodosRelatedToSpring("dummyUser");
+		
+		assertEquals("should return 3 spring named objects", 0, filteredTodoList.size());
+	}	
+	
+	
+	
+	
+	/*
+	@Test
 	void testRetrieveTodosRelatedToSpring_UsingStub() {
 		ITodoService todoServiceStub = new TodoServiceStub();
 		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceStub);
@@ -35,6 +74,6 @@ public class TodoBusinessImplStubUnitTest {
 		List<String> filteredTodoList = todoBusinessImpl.retrieveTodosRelatedToSpring("dummyUser1");	
 		assertEquals("should return empty list i.e 0 objects", 1, filteredTodoList.size());
 	}
-
+*/
 
 }
